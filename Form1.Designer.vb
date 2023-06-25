bea50ff95e5fd305ef4310deea611282ae801d11
wtechsistemas <90577344+wtechsistemas@users.noreply.github.com>
2023-06-25 11:58:42 -0300

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,182 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        Buttoniniciar = New Button()
+        TextURL = New TextBox()
+        ProgressBar1 = New ProgressBar()
+        textboxdestino = New TextBox()
+        ButtonAdd = New Button()
+        ListView1 = New ListView()
+        ImageList1 = New ImageList(components)
+        Buttoncancelar = New Button()
+        Buttonremover = New Button()
+        Buttonpasta = New Button()
+        StatusLabel = New Label()
+        Buttonadicionar = New Button()
+        SuspendLayout()
+        ' 
+        ' Buttoniniciar
+        ' 
+        Buttoniniciar.Location = New Point(38, 365)
+        Buttoniniciar.Name = "Buttoniniciar"
+        Buttoniniciar.Size = New Size(91, 58)
+        Buttoniniciar.TabIndex = 0
+        Buttoniniciar.Text = "Iniciar"
+        Buttoniniciar.UseVisualStyleBackColor = True
+        ' 
+        ' TextURL
+        ' 
+        TextURL.Location = New Point(12, 12)
+        TextURL.Name = "TextURL"
+        TextURL.Size = New Size(619, 23)
+        TextURL.TabIndex = 1
+        ' 
+        ' ProgressBar1
+        ' 
+        ProgressBar1.Location = New Point(232, 365)
+        ProgressBar1.Name = "ProgressBar1"
+        ProgressBar1.Size = New Size(399, 23)
+        ProgressBar1.TabIndex = 2
+        ' 
+        ' textboxdestino
+        ' 
+        textboxdestino.Location = New Point(12, 42)
+        textboxdestino.Name = "textboxdestino"
+        textboxdestino.Size = New Size(619, 23)
+        textboxdestino.TabIndex = 3
+        ' 
+        ' ButtonAdd
+        ' 
+        ButtonAdd.Location = New Point(637, 12)
+        ButtonAdd.Name = "ButtonAdd"
+        ButtonAdd.Size = New Size(33, 23)
+        ButtonAdd.TabIndex = 4
+        ButtonAdd.Text = "+"
+        ButtonAdd.UseVisualStyleBackColor = True
+        ' 
+        ' ListView1
+        ' 
+        ListView1.LargeImageList = ImageList1
+        ListView1.Location = New Point(12, 84)
+        ListView1.Name = "ListView1"
+        ListView1.Size = New Size(619, 248)
+        ListView1.SmallImageList = ImageList1
+        ListView1.TabIndex = 5
+        ListView1.UseCompatibleStateImageBehavior = False
+        ListView1.View = View.Details
+        ' 
+        ' ImageList1
+        ' 
+        ImageList1.ColorDepth = ColorDepth.Depth8Bit
+        ImageList1.ImageStream = CType(resources.GetObject("ImageList1.ImageStream"), ImageListStreamer)
+        ImageList1.TransparentColor = Color.Transparent
+        ImageList1.Images.SetKeyName(0, "3876348_exe_executable_extension_file_icon.ico")
+        ImageList1.Images.SetKeyName(1, "3876325_compressed_extension_file_z_icon.ico")
+        ImageList1.Images.SetKeyName(2, "1608474_file_o_picture_icon.ico")
+        ImageList1.Images.SetKeyName(3, "7267725_ext_file_generic_icon.ico")
+        ImageList1.Images.SetKeyName(4, "4634457_interface_loading_sand_sandwatch_waiting_icon.ico")
+        ImageList1.Images.SetKeyName(5, "9044028_checkbox_checked_filled_icon.ico")
+        ' 
+        ' Buttoncancelar
+        ' 
+        Buttoncancelar.Location = New Point(135, 365)
+        Buttoncancelar.Name = "Buttoncancelar"
+        Buttoncancelar.Size = New Size(91, 58)
+        Buttoncancelar.TabIndex = 6
+        Buttoncancelar.Text = "Cancelar"
+        Buttoncancelar.UseVisualStyleBackColor = True
+        ' 
+        ' Buttonremover
+        ' 
+        Buttonremover.Location = New Point(636, 88)
+        Buttonremover.Name = "Buttonremover"
+        Buttonremover.Size = New Size(34, 23)
+        Buttonremover.TabIndex = 7
+        Buttonremover.Text = "X"
+        Buttonremover.UseVisualStyleBackColor = True
+        ' 
+        ' Buttonpasta
+        ' 
+        Buttonpasta.Location = New Point(637, 44)
+        Buttonpasta.Name = "Buttonpasta"
+        Buttonpasta.Size = New Size(33, 21)
+        Buttonpasta.TabIndex = 8
+        Buttonpasta.Text = "..."
+        Buttonpasta.UseVisualStyleBackColor = True
+        ' 
+        ' StatusLabel
+        ' 
+        StatusLabel.AutoSize = True
+        StatusLabel.Location = New Point(235, 402)
+        StatusLabel.Name = "StatusLabel"
+        StatusLabel.Size = New Size(41, 15)
+        StatusLabel.TabIndex = 9
+        StatusLabel.Text = "Label1"
+        ' 
+        ' Buttonadicionar
+        ' 
+        Buttonadicionar.Location = New Point(638, 123)
+        Buttonadicionar.Name = "Buttonadicionar"
+        Buttonadicionar.Size = New Size(40, 86)
+        Buttonadicionar.TabIndex = 10
+        Buttonadicionar.Text = "Button1"
+        Buttonadicionar.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(690, 450)
+        Controls.Add(Buttonadicionar)
+        Controls.Add(StatusLabel)
+        Controls.Add(Buttonpasta)
+        Controls.Add(Buttonremover)
+        Controls.Add(Buttoncancelar)
+        Controls.Add(ListView1)
+        Controls.Add(ButtonAdd)
+        Controls.Add(textboxdestino)
+        Controls.Add(ProgressBar1)
+        Controls.Add(TextURL)
+        Controls.Add(Buttoniniciar)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Buttoniniciar As Button
+    Friend WithEvents TextURL As TextBox
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents textboxdestino As TextBox
+    Friend WithEvents ButtonAdd As Button
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents Buttoncancelar As Button
+    Friend WithEvents Buttonremover As Button
+    Friend WithEvents Buttonpasta As Button
+    Friend WithEvents StatusLabel As Label
+    Friend WithEvents ImageList1 As ImageList
+    Friend WithEvents Buttonadicionar As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,262 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Xml
+
+Public Class Form1
+    Private settingsFilePath As String = "settings.xml"
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Configurar o ListView como exibição de tabela
+        ListView1.View = View.Details
+
+        ' Adicionar as colunas ao ListView
+        ListView1.Columns.Add("URL", 200)
+        ListView1.Columns.Add("Nome do Arquivo", 160)
+        ListView1.Columns.Add("Tamanho", 80)
+        ListView1.Columns.Add("Status", 80)
+
+        ' Carregar as configurações do arquivo XML, se existir
+        LoadSettingsFromFile()
+
+        ' Habilitar o botão Cancelar
+        Buttoncancelar.Enabled = False
+
+        ' Habilitar a capacidade de arrastar e soltar arquivos no ListView
+        ListView1.AllowDrop = True
+        AddHandler ListView1.DragEnter, AddressOf ListView1_DragEnter
+        AddHandler ListView1.DragDrop, AddressOf ListView1_DragDrop
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        ' Salvar as configurações no arquivo XML
+        SaveSettingsToFile()
+    End Sub
+
+    Private Sub ButtonAdd_Click(sender As Object, e As EventArgs) Handles ButtonAdd.Click
+        ' Obter a URL do vídeo
+        Dim url As String = TextURL.Text
+
+        ' Obter o nome do arquivo a partir da URL
+        Dim fileName As String = Path.GetFileName(url)
+
+        ' Obter o tamanho do arquivo
+        Dim fileSize As String = GetFileSize(url)
+
+        ' Definir o ícone com base na extensão do arquivo
+        Dim fileExtension As String = Path.GetExtension(fileName).ToLower()
+        Dim imageIndex As Integer
+        Select Case fileExtension
+            Case ".exe"
+                imageIndex = 0
+            Case ".zip", ".rar", ".iso"
+                imageIndex = 1
+            Case ".jpg", ".bmp", ".png", ".gif"
+                imageIndex = 2
+            Case Else
+                imageIndex = 3
+        End Select
+
+        ' Adicionar o link, nome do arquivo, tamanho e status ao ListView
+        Dim item As New ListViewItem({url, fileName, fileSize, "Aguardando"})
+        item.ImageIndex = imageIndex
+
+        ListView1.Items.Add(item)
+    End Sub
+
+    Private Function GetFileSize(url As String) As String
+        Try
+            Using webClient As New WebClient()
+                Dim fileInfo = webClient.OpenRead(url)
+                Dim sizeInBytes = Convert.ToInt64(webClient.ResponseHeaders("Content-Length"))
+                Dim sizeInMB = Math.Round(sizeInBytes / (1024.0 * 1024.0), 2)
+                Return $"{sizeInMB} MB"
+            End Using
+        Catch ex As Exception
+            Return "N/A"
+        End Try
+    End Function
+
+    Private Sub ButtonPasta_Click(sender As Object, e As EventArgs) Handles Buttonpasta.Click
+        ' Abrir o diálogo de seleção de pasta
+        Dim dialog As New FolderBrowserDialog()
+        If dialog.ShowDialog() = DialogResult.OK Then
+            textboxdestino.Text = dialog.SelectedPath
+        End If
+    End Sub
+
+    Private Sub ButtonRemover_Click(sender As Object, e As EventArgs) Handles Buttonremover.Click
+        ' Remover o item selecionado do ListView
+        If ListView1.SelectedItems.Count > 0 Then
+            ListView1.Items.Remove(ListView1.SelectedItems(0))
+        End If
+    End Sub
+
+    Private Sub ButtonCancelar_Click(sender As Object, e As EventArgs) Handles Buttoncancelar.Click
+        ' Cancelar os downloads
+        For Each item As ListViewItem In ListView1.Items
+            If item.Tag IsNot Nothing AndAlso TypeOf item.Tag Is WebClient Then
+                DirectCast(item.Tag, WebClient).CancelAsync()
+            End If
+        Next
+
+        ' Habilitar o botão Iniciar novamente
+        Buttoniniciar.Enabled = True
+
+        ' Desabilitar o botão Cancelar
+        Buttoncancelar.Enabled = False
+    End Sub
+
+    Private Sub ButtonIniciar_Click(sender As Object, e As EventArgs) Handles Buttoniniciar.Click
+        ' Desabilitar o botão Iniciar
+        Buttoniniciar.Enabled = False
+
+        ' Habilitar o botão Cancelar
+        Buttoncancelar.Enabled = True
+
+        ' Iniciar os downloads
+        For Each item As ListViewItem In ListView1.Items
+            If item.Tag Is Nothing Then
+                Dim url As String = item.SubItems(0).Text
+                Dim fileName As String = item.SubItems(1).Text
+                Dim destinationPath As String = Path.Combine(textboxdestino.Text, fileName)
+
+                Dim webClient As New WebClient()
+                AddHandler webClient.DownloadProgressChanged, AddressOf WebClient_DownloadProgressChanged
+                AddHandler webClient.DownloadFileCompleted, AddressOf WebClient_DownloadFileCompleted
+
+                webClient.DownloadFileAsync(New Uri(url), destinationPath, item)
+
+                item.Tag = webClient
+            End If
+        Next
+    End Sub
+
+    Private Sub WebClient_DownloadProgressChanged(sender As Object, e As DownloadProgressChangedEventArgs)
+        Dim webClient As WebClient = DirectCast(sender, WebClient)
+        Dim item As ListViewItem = DirectCast(e.UserState, ListViewItem)
+
+        ' Atualizar a ProgressBar
+        item.SubItems(3).Text = "Baixando"
+        ProgressBar1.Value = e.ProgressPercentage
+
+        ' Atualizar o status do download
+        Dim downloadedSize = Math.Round(e.BytesReceived / (1024.0 * 1024.0), 2)
+        Dim totalSize = Math.Round(e.TotalBytesToReceive / (1024.0 * 1024.0), 2)
+        Dim speed = Math.Round(e.BytesReceived / (1024.0 * e.ProgressPercentage / 100), 2)
+        StatusLabel.Text = $"Baixando o arquivo {ListView1.Items.IndexOf(item) + 1} de {ListView1.Items.Count} ({downloadedSize} MB de {totalSize} MB, Velocidade: {speed} KB/s)"
+
+        ' Atualizar o ícone para o ícone em andamento (índice 3) do ImageList
+        item.ImageIndex = 4
+    End Sub
+
+    Private Sub WebClient_DownloadFileCompleted(sender As Object, e As System.ComponentModel.AsyncCompletedEventArgs)
+        Dim webClient As WebClient = DirectCast(sender, WebClient)
+        Dim item As ListViewItem = DirectCast(e.UserState, ListViewItem)
+
+        ' Verificar se o download foi concluído com sucesso
+        If e.Error Is Nothing AndAlso Not e.Cancelled Then
+            ' Atualizar o status do download
+            item.SubItems(3).Text = "Concluído"
+
+            ' Atualizar o ícone para o ícone de conclusão (índice 5) do ImageList
+            item.ImageIndex = 5
+        Else
+            ' Atualizar o status do download
+            item.SubItems(3).Text = "Erro"
+
+            ' Atualizar o ícone para o ícone de erro (índice 6) do ImageList
+            item.ImageIndex = 6
+        End If
+
+        ' Remover o WebClient associado ao item
+        item.Tag = Nothing
+
+        ' Verificar se todos os downloads foram concluídos
+        If ListView1.Items.Cast(Of ListViewItem)().All(Function(i) i.SubItems(3).Text <> "Baixando") Then
+            ' Habilitar o botão Iniciar novamente
+            Buttoniniciar.Enabled = True
+
+            ' Desabilitar o botão Cancelar
+            Buttoncancelar.Enabled = False
+
+            ' Exibir mensagem de conclusão
+            MessageBox.Show("Todos os downloads foram concluídos.")
+        End If
+    End Sub
+
+    Private Sub ListView1_DragEnter(sender As Object, e As DragEventArgs)
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            e.Effect = DragDropEffects.Copy
+        End If
+    End Sub
+
+    Private Sub ListView1_DragDrop(sender As Object, e As DragEventArgs)
+        Dim files() As String = CType(e.Data.GetData(DataFormats.FileDrop), String())
+
+        For Each file In files
+            If Path.GetExtension(file).ToLower() = ".txt" Then
+                Dim urls As List(Of String) = GetUrlsFromFile(file)
+
+                For Each url In urls
+                    Dim fileName As String = Path.GetFileName(url)
+                    Dim fileSize As String = GetFileSize(url)
+                    Dim imageIndex As Integer = 3 ' Índice do ícone padrão
+
+                    Dim item As New ListViewItem({url, fileName, fileSize, "Aguardando"})
+                    item.ImageIndex = imageIndex
+
+                    ListView1.Items.Add(item)
+                Next
+            End If
+        Next
+    End Sub
+
+    Private Function GetUrlsFromFile(filePath As String) As List(Of String)
+        Dim urls As New List(Of String)()
+
+        Try
+            Using reader As New StreamReader(filePath)
+                While Not reader.EndOfStream
+                    Dim line As String = reader.ReadLine().Trim()
+
+                    If line.StartsWith("http://") OrElse line.StartsWith("https://") Then
+                        urls.Add(line)
+                    End If
+                End While
+            End Using
+        Catch ex As Exception
+            MessageBox.Show("Erro ao ler o arquivo de texto: " & ex.Message)
+        End Try
+
+        Return urls
+    End Function
+
+    Private Sub LoadSettingsFromFile()
+        If File.Exists(settingsFilePath) Then
+            Try
+                Dim doc As New XmlDocument()
+                doc.Load(settingsFilePath)
+
+                textboxdestino.Text = doc.SelectSingleNode("/settings/destinationFolder")?.InnerText
+            Catch ex As Exception
+                MessageBox.Show("Erro ao carregar as configurações do arquivo XML: " & ex.Message)
+            End Try
+        End If
+    End Sub
+
+    Private Sub SaveSettingsToFile()
+        Try
+            Dim doc As New XmlDocument()
+            Dim rootElement As XmlElement = doc.CreateElement("settings")
+            doc.AppendChild(rootElement)
+
+            Dim destinationFolderElement As XmlElement = doc.CreateElement("destinationFolder")
+            destinationFolderElement.InnerText = textboxdestino.Text
+            rootElement.AppendChild(destinationFolderElement)
+
+            doc.Save(settingsFilePath)
+        Catch ex As Exception
+            MessageBox.Show("Erro ao salvar as configurações no arquivo XML: " & ex.Message)
+        End Try
+    End Sub
+End Class
